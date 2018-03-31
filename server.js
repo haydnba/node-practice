070d12f26092d7a79f99497912224bda733e18a7
HA <haydnba@gmail.com>
2018-03-31 18:04:35 +0100

    refactor 'server.js' and add basic 'index.js'

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
-}).listen(8888);
+const start = function() {
+  const server = http.createServer(function(request, response) {
+    console.log('Request Received...');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World...');
+    response.end();
+  });
+  server.listen(8888);
+  console.log('Server Started...');
+}
+
+module.exports = { start };
+
+
+
